ed5dff0e0dfb20b2eb1818a815fea52f0e27f4e8
sporti <thekevinvigil@gmail.com>
2024-02-24 13:02:09 -0600

    Cleaned

--- data-finding/src/PricePredictor.py
+++ data-finding/src/PricePredictor.py
@@ -13,30 +13,18 @@ arrYAxis =[]
 
 #Graph a CSV given an X and Y axis index
 def graphXY(filePath, xIndex: int, yIndex: int):
+  #Creates example data and runs it. not complete
+  df = pd.read_csv("data-finding/data/RDC_Inventory_Core_Metrics_Zip_History_processed_files/75082data.csv")
   df.plot(kind = "scatter", x = "date", y = "median_price")
   plot.show()
-#   with open(filePath, newline="") as csvFile:
-#     reader = csv.reader(csvFile, delimiter=',', quotechar='"')
-#     reader.__next__() #Skip TOC
-#     if xIndex != -1 and yIndex != -1:
-#       arrXAxis = []
-#       arrYAxis = []
-#       for row in reader:
-#         arrXAxis.append(row[xIndex])
-#         arrYAxis.append(int(row[yIndex]))
-#       arrXAxis = arrXAxis[::-1]
-#       arrYAxis = arrYAxis[::-1]
-#       plot.plot(arrXAxis,arrYAxis, "ro")
-#       # plot.axis((201600, 202312, 300000,700000))
-#       plot.show()
 
 #To give an example of what the dataset looks like, and the output of the program, I have put the csv of my childhood zipcode.
 #This will be used for all examples. Please keep in mind, this is a complete dataset (July 2016-December 2023).
 #Not all datasets are complete
 
-
+#Turned this into a fuctnion
 def linearRegression() -> None:
-  print(os.getcwd())
+  #print(os.getcwd())
   df = pd.read_csv("data-finding/data/RDC_Inventory_Core_Metrics_Zip_History_processed_files/75082data.csv")
   df['date'] = pd.to_datetime(df["date"], yearfirst = True)
   #Preprocessing
@@ -93,71 +81,10 @@ def linearRegression() -> None:
   ax.set_xlabel("Date")
   ax.set_ylabel("Median Price")
   plot.show()
-# graphXY(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-# graphXY(File, x-axis-index, y-axis-index)
+
+
 graphXY("77433data.csv", 0, 4)
+linearRegression()
 
 
 # reg.score(x_train,y_train)
-
-# import matplotlib.pyplot as plot
-# import numpy as np
-# import csv
-# 
-# import datetime
-
-# #HELPER: Find indexes of given x and y axis
-# def indexFinder(reader, x, y):
-#   xIndex = -1
-#   yIndex = -1
-#   menu = reader.__next__()
-#   for i in range(len(menu)):
-#     if x == menu[i]:
-#       xIndex = i
-#     if y == menu[i]:
-#       yIndex = i
-#   return (xIndex, yIndex)
-
-# #HELPER: create array from csv given index to grab from
-# def createArray(reader, index: int):
-#   axis = []
-#   for row in reader:
-#     axis.append(row[index])
-#   return axis
-
-
-
-# #Run linear regression on an CSV based on given x and y axis
-# def linearRegression():
-#   return False
-
-# #HELPER: turn string into date
-# def convertDate(date: str):
-#   year = date[0:4]
-#   month = date[4:]
-#   return datetime.date(int(year),int(month),1)
-
-# #Graph a CSV given an X and Y axis index
-# def graphXY(filePath, xIndex: int, yIndex: int):
-#   with open(filePath, newline="") as csvFile:
-#     reader = csv.reader(csvFile, delimiter=',', quotechar='"')
-#     if xIndex != -1 and yIndex != -1:
-#       arrXAxis = []
-#       arrYAxis = []
-#       for row in reader:
-#         arrXAxis.append(convertDate(row[xIndex]))
-#         arrYAxis.append(int(row[yIndex]))
-#       arrXAxis = arrXAxis[::-1]
-#       arrYAxis = arrYAxis[::-1]
-#       plot.plot(arrXAxis,arrYAxis, "ro")
-#       # plot.axis((201600, 202312, 300000,700000))
-#       plot.show()
-
-
-
-# if __name__=='__main__':
-  # print ('argument list', sys.argv)
-  #arg 1: file
-  #arg 2: 0
-  #arg 3: 4
-  # graphXY(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
