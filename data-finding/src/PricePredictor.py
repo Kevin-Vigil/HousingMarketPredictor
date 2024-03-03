aaf9fd049904f8126cc40d191842e67459ecad6e
sporti <thekevinvigil@gmail.com>
2024-03-02 23:08:01 -0600

    Generalizing pricePredictor

--- data-finding/src/PricePredictor.py
+++ data-finding/src/PricePredictor.py
@@ -2,14 +2,13 @@ import matplotlib.pyplot as plot
 import numpy as np
 import datetime
 import pandas as pd
-
-arrXAxis =[]
-arrYAxis =[]
-
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import logging
+import os
 
 #Graph a CSV given an X and Y axis index
-def graphXY(filePath, xIndex: int, yIndex: int):
+def graphXY(filePath, xIndex: int, yIndex: int) -> None:
   #Creates example data and runs it. not complete
   df = pd.read_csv("data-finding/data/RDC_Inventory_Core_Metrics_Zip_History_processed_files/75082data.csv")
   df.plot(kind = "scatter", x = "date", y = "median_price")
@@ -20,49 +19,48 @@ def graphXY(filePath, xIndex: int, yIndex: int):
 #Not all datasets are complete
 
 #Turned this into a fuctnion
-def linearRegression(file: str) -> None:
-  #print(os.getcwd())
-  df = pd.read_csv("data-finding/data/RDC_Inventory_Core_Metrics_Zip_History_processed_files/75082data.csv")
+def linearRegression(file: str) -> plot.figure:
+  ab_path = os.path.dirname(__file__) #Grabs the location of the file and joins with desired file to create absolute path
+  desired_path = os.path.join(ab_path, file)
+  logging.debug(desired_path) #Extra info for debugging
+  df = pd.read_csv(desired_path)
   df['date'] = pd.to_datetime(df["date"], yearfirst = True)
   #Preprocessing
   cols = ["postal_code", "location_name", "active_listing_count", "median_days_on_market","median_sqft","average_price","total_listing_count","quality_flag"]
   df = df.drop(cols, axis=1)
-  df.info()
+  # df.info() #Extra info for debugging
 
   #Either drop or create normalized values for null values
   #Dropping because median listing price is important
   df = df.dropna()
-  df.info()
+  # df.info() #Extra info for debugging
 
-  from sklearn.model_selection import train_test_split
+
 
   #transfer to usable data
-
-
   df.date = pd.to_datetime(df.date)
   x = df.date.values
   # x_temp = dateTransfer(x)
   y = df['median_price'].values
-  print(x)
-  print(y)
+  logging.info(x)
+  logging.info(y)
   # x = np.delete(x, 1, axis = 1)
 
   x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
-  plot.plot(x_train, y_train, 'ro')
-  plot.title("Train data")
-  plot.show()
+  # plot.plot(x_train, y_train, 'ro')
+  # plot.title("Train data")
+  # plot.show()
 
-  print(x_train)
-  print(y_train)
+  logging.info(x_train)
+  logging.info(y_train)
 
-  plot.plot(x_test, y_test, 'ro')
-  plot.title("Test data")
-  plot.show()
-  print(x_test)
-  print(y_test)
+  # plot.plot(x_test, y_test, 'ro')
+  # plot.title("Test data")
+  # plot.show()
+  logging.info(x_test)
+  logging.info(y_test)
 
-  from sklearn.linear_model import LinearRegression
 
   reg = LinearRegression()# Create the model
   x_train = x_train.reshape(-1,1) # Reshape the data to a 2D array as it is currently a 1D array
@@ -77,11 +75,13 @@ def linearRegression(file: str) -> None:
   ax.set_title("Prediction of Zip code 77433")
   ax.set_xlabel("Date")
   ax.set_ylabel("Median Price")
-  plot.show()
+  logging.info(reg.score(df.date.values.astype(float).reshape(-1,1),df['median_price'].values))
+  return ax
 
 
-graphXY("77433data.csv", 0, 4)
-linearRegression()
+# graphXY("77433data.csv", 0, 4)
+ax = linearRegression("../data/75082data.csv")
+plot.show()
 
 
 # reg.score(x_train,y_train)
